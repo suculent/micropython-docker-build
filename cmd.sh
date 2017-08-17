32fb270eb89e5a402d642034a0176c895e859764
Matěj Sychra <suculent@me.com>
2017-08-17 13:42:18 +0200

    support for thinx.yml; deletes disabled firmware modules before building

--- cmd.sh
+++ cmd.sh
@@ -8,4 +8,38 @@ PATH=/esp-open-sdk/xtensa-lx106-elf/bin:$PATH
 
 cd micropython/mpy-cross && make
 
-cd micropython/esp8266 && make axtls && make
+# Parse thinx.yml config
+
+PLATFORM="esp8266"
+
+if [[ -f "thinx.yml" ]]; then
+  echo "Reading thinx.yml:"
+  parse_yaml thinx.yml
+  eval $(parse_yaml thinx.yml)
+
+  if [[ ! -z ${micropython_platform} ]]; then
+    PLATFORM=${micropython_platform}
+  fi
+fi
+
+cd micropython/$PLATFORM
+
+if [[ ! -z ${micropython_modules[@]} ]]; then
+  pushd modules
+  MODULES=$(ls -l *.py)
+  echo "- modules: ${micropython_modules[@]}"
+  for module in ${micropython_modules[@]} do
+    if [[ "module" == "_boot.py" ]]; then
+      break;
+    fi
+    if [[ $MODULES == "*${module}*"]]; then
+      echo "Enabling Micropython module ${module}"
+    else
+      echo "Disabling Micrphython module ${module}"
+      rm -rf ${module}
+    fi
+  done
+  popd
+fi
+
+make axtls && make
